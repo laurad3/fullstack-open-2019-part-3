d9c92f9cee68c06e5d32ce35f2bba8758086b52d
laurad3 <lauralvicuna@gmail.com>
2020-03-22 16:53:11 +0200

    add build directory

---
+++ build/precache-manifest.88f01f59c257b126c45caa7a3c0b11ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e1ad79c8538aa210f01e7974d57784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d143a1339b02c9a35f5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "841753c3633e69994396",
+    "url": "/static/js/2.48591e28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.48591e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d143a1339b02c9a35f5",
+    "url": "/static/js/main.f4334b66.chunk.js"
+  },
+  {
+    "revision": "85f5b3e81da88a0bfe4f",
+    "url": "/static/js/runtime-main.b41cb28e.js"
+  }
+]);
